32520da32e59bb75dcbaee353fe6833f324c7805
Joe Cummings <jrcummings27@gmail.com>
2019-02-02 17:01:50 -0600

    i am an idiot

--- game_scaper.py
+++ game_scaper.py
@@ -136,7 +136,7 @@ class GameScraper(object):
     def insertIntoDb(self):
         print('Cleaning database')
 
-        conn = psycopg2.connect("host="+os.eviron['pghost'] + " dbname="+os.environ['pgdb'] + " user="+os.environ['pguser'] + " password="+os.environ['pgpassword'] + " port="+os.environ['pgport'])
+        conn = psycopg2.connect("host="+os.environ['pghost'] + " dbname="+os.environ['pgdb'] + " user="+os.environ['pguser'] + " password="+os.environ['pgpassword'] + " port="+os.environ['pgport'])
         cur = conn.cursor()
 
         cur.execute("""
